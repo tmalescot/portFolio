e73967a00200663c1f8e56ff7c4e27f54e6ab79d
Thibaut MALESCOT <tmalescot@B4-04.btssio.fr>
2019-04-11 10:50:10 +0200

    Jeu 11 Avril 10h49

---
+++ resources/views/index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>thibautmalescot.fr</title>
+    <link href="https://fonts.googleapis.com/css?family=Maitree:300,400,500,600" rel="stylesheet">
+  </head>
+  <body>
+    <section class="indexFondPleinEcran">
+      <div class="owerlay">
+        <ul class="ulNavigate">
+          <li class="liNavigate"><a class="negative" href="#traineeship">[ Traineeship ]</a></li>
+          <li class="liNavigate"><a class="negative" href="#projects">[ Projects ]</a></li>
+          <li class="liNavigate"><a class="negative" href="#techWatch">[ Tech Watch ]</a></li>
+          <li class="liNavigate"><a class="negative" href="#contact">[ Contact ]</a></li>
+        </ul>
+        <div class="indexBandeau">
+          <p class="indexTextBandeau">MALESCOT THIBAUT - Web Programmer - BTS SIO Student</p>
+        </div>
+      </div>
+    </section>
+    <section class="body">
+      <section class="article artImpair">
+        <h1 class="titleArticle">Présentation de l'ensemble des compétences</h1>
+        <p class="textArticle">
+          Dans le cadre de ma formation, j’ai recenser les différents projets que j’ai pu traiter en cours, personnellement et en stage
+          dans une feuille de calcul où chaque projet correspond à une ligne et valide un certain nombre de compétence. Avec un total de
+          52 compétences, 4 obligatoires et 48 facultatives, bien sûr, plus la liste des compétences validés l’allonge, mieux c’est !
+          Vous pouvez voir cette fiche en <a href="../public/pdf/tableauCompétence.pdf">cliquant ici</a>.
+        </p>
+        <p class="textArticle">
+          Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset
+          in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes
+          arcanorum soli conscii timebantur.
+          Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset
+          in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes
+          arcanorum soli conscii timebantur.
+        </p>
+        <p class="textArticle">
+          Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset
+          in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes
+          arcanorum soli conscii timebantur.
+          Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset
+          in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes
+          arcanorum soli conscii timebantur.
+        </p>
+      </section>
+      <section class="article artPair">
+        <h1 class="titleArticle" id="traineeship">Compte rendu de l'ensemble des stages</h1>
+        <p class="textArticle">
+          Dans l’objectif d’obtenir mon diplôme, j’ai dû effectuer deux stages pour une durée total de 11 semaines (6 semaines du Lundi
+          28 Mai 2018 au Vendredi 06 Juillet 2018, et, 5 semaines du Lundi 14 janvier 2019 au Vendredi 15 Février 2019). Ces stages
+          permettent une réel mise en situations avec des contraintes plus durs, des clients avec des souhaits, des attendus, un cahier
+          des charges, une date de rendu de projet, un suivi de la part du client, etc..
+        </p>
+        <p class="textArticle">
+          Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset
+          in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes
+          arcanorum soli conscii timebantur.
+        </p>
+        <div class="resumStage">
+          <div class="stage">
+            <h2 class="titleStage">Stage de première année</h2>
+            <p class="textArticle">
+              <ul class="ulPresentStage">
+                <li>Entreprise : Pierre Pancratio</li>
+                <li>Lieu : Guéret</li>
+                <li>Activité : Refonte d'un site web</li>
+              </ul>
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+            </p>
+          </div>
+          <div class="stage">
+            <h2 class="titleStage">Stage de seconde année</h2>
+            <p class="textArticle">
+              <ul class="ulPresentStage">
+                <li>Entreprise : M2S33 Formations</li>
+                <li>Lieu : Bordeaux</li>
+                <li>Activité : Création d'un site web</li>
+              </ul>
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="article artImpair">
+        <h1 class="titleArticle" id="projects">Mes projets</h1>
+        <p class="textArticle">
+          Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+        </p>
+        <div class="resumProjects">
+          <div class="projects">
+            <h2 class="titleProjects">Site M2S33 Formations</h2>
+            <p class="textArticle">
+              <ul class="ulPresentProjects">
+                <li>Type : Site Web</li>
+                <li>Languages : HTML/CSS/PHP/SQL/JS</li>
+                <li>Description : Création d'un site web vitrine dynamique modifiable</li>
+              </ul>
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+            </p>
+          </div>
+          <div class="projects">
+            <h2 class="titleProjects">Application de gestion banquaire</h2>
+            <p class="textArticle">
+              <ul class="ulPresentProjects">
+                <li>Type : Application "Client Lourd"</li>
+                <li>Languages : JAVA/XML</li>
+                <li>Description : Création d'une application de gestion banquaire, d'aide à la dépense</li>
+              </ul>
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="article artPair">
+        <h1 class="titleArticle" id="techWatch">Veille technologique</h1>
+        <p class="textArticle">
+          Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+        </p>
+        <div class="resumTechWatch">
+          <div class="imgTechWatch">
+            <img src="images/imgTechWatch.jpg">
+          </div>
+          <div class="techWatch">
+            <h2 class="titleSujetVeille">Le sujet de la veille</h2>
+            <p class="textArticle">
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim.
+            </p>
+            <p class="textArticle">
+              Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="article artImpair">
+        <h1 class="titleArticle" id="contact">Contact</h1>
+        <p class="textArticle">
+          <form action="cible" method="post">
+            <fieldset>
+              <legend>Envoyer un mail</legend>
+              <!--Nom-->
+              <label for="firstName">Nom :</label><br />
+              <input type="text" name="firstName" id="firstName" maxlength="50" required /><br /><br />
+              <!--Prénom-->
+              <label for="name">Prénom :</label><br />
+              <input type="text" name="name" id="name" maxlength="20" required /><br /><br />
+              <!--Adresse email-->
+              <label for="mail">Mail :</label><br />
+              <input type="text" name="mail" id="mail" required /><br /><br />
+              <!--N° de téléphone-->
+              <label for="number">Numéro :</label><br />
+              <input type="text" name="number" id="number" maxlength="10" required /><br /><br />
+              <!--Le sujet de votre demande-->
+              <label for="sujetDemande">Le sujet de votre demande :</label> <br />
+							<select	name="sujetDemande" id="sujetDemande">
+								<option value="formationEnt" selected> Création d'un site web </option>
+								<option value="formationPerso"> Refonte d'un site web </option>
+								<option value="demandeurEmploi"> Contact professionnel </option>
+								<option value="autre"> Autre... </option>
+							</select> <br /><br />
+              <!--Votre demande-->
+							<label for="demande">Votre demande :</label> <br />
+							<textarea name="demande" id="demande" cols="120" rows="10" required></textarea><br /><br />
+              <!--Envoi-->
+              <input type="submit" class="btnEnvoyer" value="Envoyer">
+            </fieldset>
+          </form>
+        </p>
+      </section>
+    </section>
+    <section class="footer">
+      <div class="resumFooter">
+        <div>
+          <h2 class="titleSujetVeille">Le sujet de la veille</h2>
+          <p class="textArticle">
+            Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+          </p>
+        </div>
+        <div>
+          <h2 class="titleSujetVeille">Le sujet de la veille</h2>
+          <p class="textArticle">
+            Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.
+          </p>
+        </div>
+        <div>
+          <h2 class="titleFooter">Réseaux Sociaux</h2>
+          <div class="displayerLogo">
+            <div>
+              <img src="images/iconsFacebook.png" alt="logo facebook">
+            </div>
+            <div>
+              <img src="images/iconsTwitter.png" alt="logo twitter">
+            </div>
+          </div>
+          <div class="displayerLogo">
+            <div>
+              <img src="images/iconsGoogle.png" alt="logo linkedin">
+            </div>
+            <div>
+              <img src="images/iconsLinkedin.png" alt="logo google+">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mentionLegale">
+      <p>
+        © Copyright 2019 - thibautmalescot.fr - Malescot T. Web Programmer
+      </p>
+    </section>
+  </body>
+</html>
